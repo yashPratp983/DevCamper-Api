9c0881e98cdddcbd8509f2a40eac608f1caa5f01
Yash Pratap Singh <102533457+yashPratp983@users.noreply.github.com>
2022-12-23 13:28:08 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+    try{
     const conn = await mongoose.connect(process.env.db_url)
 
     console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold);
+    }catch(err){
+        console.log(err);
+    }
 }
 
 module.exports = connectDB;
